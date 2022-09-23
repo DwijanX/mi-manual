3c9c24727f61ee267af3caeddd5a14784f95f44d
ChristianRivero <christianriveroarnez@gmail.com>
2022-09-23 17:58:29 -0400

    Added flexbox concepts and an interactive example

--- Src/Csspresenter.js
+++ Src/Csspresenter.js
@@ -8,12 +8,20 @@ const tableSeletors=document.querySelector("#SelectorsTable")
 
 const inputUnits=document.querySelector("#UnitsInput")
 const tableUnits=document.querySelector("#UnitsTable")
+
+const displaySectInput=document.querySelector("#displaySectInput")
+const displayTable=document.querySelector("#displayTable")
+
+
 const clickAbleRows=document.querySelectorAll(".clickableRow")
 
 const CloseOverlayButtons=document.querySelectorAll(".CloseOverlayButton")
 const Overlays=document.querySelectorAll(".overlay")
 
 const TheoricButtons=document.querySelectorAll(".TheoricButton")
+
+const codeArea=document.querySelector("#flexCssCode")
+const ObjectsContainerFlexExample=document.querySelector("#ObjectsContainerFlexExample")
 
 inputColors.addEventListener("input",()=>{
     filterBasedOnInputAndColumn(inputColors,tableColors,0);
@@ -24,6 +32,11 @@ inputSelectors.addEventListener("input",()=>{
 inputUnits.addEventListener("input",()=>{
     filterBasedOnInputAndColumn(inputUnits,tableUnits,0);
 })
+displaySectInput.addEventListener("input",()=>{
+    filterBasedOnInputAndColumn(displaySectInput,displayTable,0);
+})
+
+
 clickAbleRows.forEach((clickAbleRow)=>{
     clickAbleRow.addEventListener("dblclick",()=>{
         let link=clickAbleRow.getAttribute("data-href")
@@ -44,3 +57,19 @@ TheoricButtons.forEach((TheoricButton)=>{
         relatedOverlay.style.display="flex"
     })
 })
+function setStyles(element, styles)
+{
+    for(var s in styles) {
+        element.style[s] = styles[s];
+    }
+}
+codeArea.addEventListener("input",()=>{
+    let CssString=codeArea.value
+    CssString=CssString.replaceAll(";",",")
+    let StylesObj=JSON.parse(CssString)
+    setStyles(ObjectsContainerFlexExample,StylesObj)
+})
+addEventListener('load', ()=>{
+    let currentCSSCode='{'+'\r\n'+'"display":"flex";'+"\r\n"+'"flex-direction": "row";'+"\r\n"+'"align-items": "center";'+"\r\n"+'"flex-wrap": "nowrap";'+"\r\n"+'"justify-content": "center"'+"\r\n"+"}"
+    codeArea.innerHTML=currentCSSCode
+});
